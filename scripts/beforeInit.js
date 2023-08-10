d68e1e6c94cf4b9fc1b5111091ef6e3e477758da
sych74 <sych74@gmail.com>
2023-08-10 15:39:53 +0200

    WP-639

--- scripts/beforeInit.js
+++ scripts/beforeInit.js
@@ -5,6 +5,7 @@ var cdnAppid = "c05ffa5b45628a2a0c95467ebca8a0b4",
     fields = {},
     field,
     resp,
+    regions,
     LE = "le-addon",
     CDN = "cdn-addon";
 
@@ -135,6 +136,18 @@ if (parseInt("${quota.environment.maxcount:0}", 10) <= resp.infos.length) {
           "required": true,
       }]
   });
+}
+
+var regions = jelastic.env.control.GetRegions(appid, session);
+if (regions.result != 0) return regions;
+
+
+if (regions.array.length < 2) {
+  markup = "Package cannot be installed on less than 2 regions. Please contact support or choose a provider with more regions";
+  settings.fields.push(
+    {"type": "displayfield", "cls": "warning", "height": 30, "hideLabel": true, "markup": markup},
+    {"type": "compositefield","height": 0,"hideLabel": true,"width": 0,"items": [{"height": 0,"type": "string","required": true}]}
+  );
 }
 
 
